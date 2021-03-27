673ac2187de6541abd6fcda3002a368a675b7d6c
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-03-27 15:23:34 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,14 +25,15 @@ class Triangle
     def triangle_equality
       
       #sum lengths of any two sides <= third side or any side <= 0, raise TriangleError
-      #real_triangle = [(@length_a + @length_b > @length_c), (@length_a + @length_c > @length_b), (@length_b + @length_c > @length_a)]
-      # if @length_a == 0 || @length_b == 0 || @length_c == 0
-      #   raise TriangleError
-      # end
+      real_triangle = [(@length_a + @length_b > @length_c), (@length_a + @length_c > @length_b), (@length_b + @length_c > @length_a)]
+      if @length_a == 0 || @length_b == 0 || @length_c == 0
+        raise TriangleError
+      end
     end
 ​
     class TriangleError < StandardError
     end
       
-​
+    #this is a comment
+    
 end
